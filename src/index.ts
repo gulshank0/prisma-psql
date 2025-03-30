e5e99d7e7628a26234c73d336b92d42f40e23908
gulshank0 <gulshanbahadur002@gmail.com>
2025-03-30 11:43:06 +0530

    uploaded learnings

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello World");
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function insertUser(
+  username: string,
+  password: string,
+  Firstname: string,
+  Lastname: string,
+) {
+  const res = await prisma.user.create({
+    data: {
+      email: username,
+      password,
+      Firstname,
+      Lastname,
+    },
+  });
+  console.log(res);
+}
+insertUser("harkirat@gmail.com", "123456", "Harkirat", "Singh");
